74fe44d6ecd02fa2b674d25ce724caee976e2f19
Francisco Romano <blastsilver@gmail.com>
2018-01-27 19:07:11 +1100

    things....

---
+++ 2018GameJam/Assets/UIMessage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UIMessage : MonoBehaviour
+{
+    private UnityEngine.UI.Text m_title;
+    private UnityEngine.UI.Text m_message;
+    public string Title { get { return m_title.text; } set { m_title.text = value; } }
+    public string Message { get { return m_message.text; } set { m_message.text = value; } }
+
+    // Use this for initialization
+    void Start ()
+    {
+        UnityEngine.UI.Text[] components = GetComponentsInChildren<UnityEngine.UI.Text>();
+        m_title = components[0];
+        m_message = components[1];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

1565e9b6ccf416f3597e193b33ea103e77086070
Mitch <mjmt997@gmail.com>
2018-01-27 22:47:32 +1100

    Planet Stuff

--- 2018GameJam/Assets/Scripts/Player/Player.cs
+++ 2018GameJam/Assets/Scripts/Player/Player.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 enum EVENT_PLAYER {
     CLICKED_ON_PLANET,
@@ -13,38 +15,84 @@ public class Player : MonoBehaviour {
 
     // acceleration
     public float m_velocity;
-    public float m_max_velocity;
-    public float m_acceleration;
-    public float m_speed;
+    float m_max_velocity = 3;
+    float m_acceleration = 0.1f;
+    float m_speed = 20;
 
-    public float m_fual = 90;
+    public float m_fual = 600;
+    public Slider m_fual_slider;
 
-    // rotation
-    public float m_rot_velocity;
-    public float m_rot_max_velocity;
-    public float m_rot_acceleration;
+    public Image m_interaction_image;
+    private float m_intaction_timer = 2;
+    private bool m_interacting = false;
+    public GameObject m_interacting_object;
 
     // private vars
     private bool m_moving;
-    
+
+    public int m_resources;
+    public int m_artifacts;
+
+    void Start () {
+        m_interaction_image.transform.parent.gameObject.SetActive(false);
+    }
     
     void Update () {
+        
+        m_fual_slider.value = m_fual / 600;
+        
+        if (Input.GetKey(KeyCode.E)) {
+            if (m_interacting_object != null) {
+                m_interacting = true;
+                m_interaction_image.transform.parent.gameObject.SetActive(true);
+
+                if (m_intaction_timer > 0) {
+                    m_intaction_timer -= Time.deltaTime;
+                    m_interaction_image.fillAmount = m_intaction_timer / 2;
+                } else {
+                    m_interacting = false;
+                    m_interaction_image.transform.parent.gameObject.SetActive(false);
+                    // interaction code...
+                    if (m_interacting_object.tag == "Planet") {
+                        m_artifacts += m_interacting_object.GetComponent<Planet>().m_artifacts;
+                        m_interacting_object.GetComponent<Planet>().m_artifacts = 0;
+
+                        m_resources += m_interacting_object.GetComponent<Planet>().m_resources;
+                        m_interacting_object.GetComponent<Planet>().m_resources = 0;
+
+                        m_fual += m_interacting_object.GetComponent<Planet>().m_fual;
+                        m_interacting_object.GetComponent<Planet>().m_fual = 0;
+                        if (m_fual > 600) m_fual = 600;
+                    }
+                }
+            }
+        } else {
+            m_intaction_timer = 2;
+            m_interaction_image.transform.parent.gameObject.SetActive(false);
+        }
 
         // Rotate
         if (Input.GetKey(KeyCode.A)) {
             this.transform.Rotate(new Vector3(0, 0, Time.deltaTime * m_speed));
+            
         }
         if (Input.GetKey(KeyCode.D)) {
             this.transform.Rotate(new Vector3(0, 0, -Time.deltaTime * m_speed));
         }
 
         // Acceleration
-        if (Input.GetKey(KeyCode.W)) {
+        if (Input.GetKey(KeyCode.W) && m_fual > 0 && !m_interacting) {
             if (m_velocity < m_max_velocity)
                 m_velocity += m_acceleration;
+            if (m_fual > 0) {
+                m_fual -= Time.deltaTime;
+            }
         } else {
-            if (Input.GetKey(KeyCode.S)) {
+            if (Input.GetKey(KeyCode.S) && m_fual > 0 && !m_interacting) {
                 m_velocity -= m_acceleration;
+                if (m_fual > 0) {
+                    m_fual -= Time.deltaTime;
+                }
             } else {
                 if (m_velocity < 0.06 && m_velocity > 0) {
                     m_velocity = 0;
@@ -65,15 +113,23 @@ public class Player : MonoBehaviour {
         } else {
             if (m_moving) {
                 __event<EVENT_PLAYER>.InvokeEvent(this, EVENT_PLAYER.MOVING, false);
+                m_interacting = false;
                 m_moving = false;
             }
         }
 
         this.transform.position += transform.up * m_velocity * Time.deltaTime;
-        this.transform.Rotate(new Vector3(0, 0, Time.deltaTime * m_rot_velocity));
     }
 
-    void OnDrawGizmos () {
-        
+    void OnTriggerEnter2D(Collider2D c) {
+        if (c.tag == "Planet") {
+            m_interacting_object = c.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D (Collider2D c) {
+        //if (c.tag != "Planet") {
+            m_interacting_object = null;
+        //}
     }
 }
---
+++ 2018GameJam/Assets/Scripts/environment/Planet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour {
+
+    public int max_resources;
+    public int min_resources;
+    public int m_resources;
+
+    public int max_artifacts;
+    public int min_artifacts;
+    public int m_artifacts;
+
+    public int max_fual;
+    public int min_fual;
+    public int m_fual;
+
+    void Start () {
+        m_resources = Random.Range(min_resources, max_resources);
+        m_artifacts = Random.Range(min_artifacts, max_artifacts);
+        m_fual = Random.Range(min_fual, max_fual);
+    }
+}

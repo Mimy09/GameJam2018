8f7e361c694632c92a6340c5916156fcd88fc1fb
Francisco Romano <blastsilver@gmail.com>
2018-01-28 15:49:05 +1100

    implemented upgrades

--- 2018GameJam/Assets/Scripts/UI/UIChoice.cs
+++ 2018GameJam/Assets/Scripts/UI/UIChoice.cs
@@ -7,19 +7,28 @@ public class UIChoice : MonoBehaviour
     // Required variables
     private Animator m_animator;
     private UnityEngine.UI.Text[] m_texts = null;
+    private UnityEngine.UI.Image[] m_images = null;
     private static int UI_CHOICE_TITLE = 0;
     private static int UI_CHOICE_ITEM_1_KEY = 2;
     private static int UI_CHOICE_ITEM_2_KEY = 4;
     private static int UI_CHOICE_ITEM_1_TEXT = 1;
     private static int UI_CHOICE_ITEM_2_TEXT = 3;
+    private static int UI_CHOICE_ITEM_1_COLOR = 1;
+    private static int UI_CHOICE_ITEM_2_COLOR = 4;
+    private static int UI_CHOICE_ITEM_1_IMAGE = 2;
+    private static int UI_CHOICE_ITEM_2_IMAGE = 5;
     private static int UI_CHOICE_ACTION_1_KEY = 6;
     private static int UI_CHOICE_ACTION_1_TEXT = 5;
 
     // Getters and Setters
     public string Tile { get { return m_texts[UI_CHOICE_TITLE].text; } set { m_texts[UI_CHOICE_TITLE].text = value; } }
     public bool Display { get { return m_animator.GetBool("Display"); } set { m_animator.SetBool("Display", value); } }
+    public Color Item1Color { get { return m_images[UI_CHOICE_ITEM_1_COLOR].color; } set { m_images[UI_CHOICE_ITEM_1_COLOR].color = value; } }
+    public Color Item2Color { get { return m_images[UI_CHOICE_ITEM_2_COLOR].color; } set { m_images[UI_CHOICE_ITEM_2_COLOR].color = value; } }
     public string Item1Text { get { return m_texts[UI_CHOICE_ITEM_1_TEXT].text; } set { m_texts[UI_CHOICE_ITEM_1_TEXT].text = value; } }
     public string Item2Text { get { return m_texts[UI_CHOICE_ITEM_2_TEXT].text; } set { m_texts[UI_CHOICE_ITEM_2_TEXT].text = value; } }
+    public Sprite Item1Image { set { m_images[UI_CHOICE_ITEM_1_IMAGE].sprite = value; } }
+    public Sprite Item2Image { set { m_images[UI_CHOICE_ITEM_2_IMAGE].sprite = value; } }
     public string Action1Key { get { return m_texts[UI_CHOICE_ITEM_1_KEY].text; } set { m_texts[UI_CHOICE_ITEM_1_KEY].text = value; } }
     public string Action2Key { get { return m_texts[UI_CHOICE_ITEM_2_KEY].text; } set { m_texts[UI_CHOICE_ITEM_2_KEY].text = value; } }
     public string Action3Key { get { return m_texts[UI_CHOICE_ACTION_1_KEY].text; } set { m_texts[UI_CHOICE_ACTION_1_KEY].text = value; } }
@@ -30,6 +39,9 @@ public class UIChoice : MonoBehaviour
     {
         // fetch components
         m_texts = GetComponentsInChildren<UnityEngine.UI.Text>();
+        m_images = GetComponentsInChildren<UnityEngine.UI.Image>();
         m_animator = GetComponent<Animator>();
+
+        Debug.Log(m_texts.Length);
 	}
 }
---
+++ 2018GameJam/Assets/Upgrades.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrades : MonoBehaviour
+{
+    private ItemStatistics m_initial = new ItemStatistics();
+    private ItemStatistics m_current = new ItemStatistics();
+    private ItemStatistics m_upgrade = new ItemStatistics();
+
+    public Color m_color1;
+    public Color m_color2;
+    public Player m_player;
+    public UIChoice m_choice;
+    public List<ItemInformation> m_items;
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+	}
+
+    void Update()
+    {
+        // check if displaying
+        if (m_choice.Display)
+        {
+            if (Input.GetKeyUp(KeyCode.E))
+            {
+                Hide();
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                m_choice.Item2Color = m_color1;
+                m_choice.Item1Color = m_color2;
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                m_choice.Item2Color = m_color2;
+                m_choice.Item1Color = m_color1;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            Show();
+        }
+    }
+
+    public void Hide()
+    {
+        m_choice.Display = false;
+    }
+    public void Show()
+    {
+        m_choice.Display = true;
+        int index = Random.Range(0, m_items.Count);
+        DisplayUpgrade2(m_items[index]);
+    }
+
+    void DisplayUpgrade2(ItemInformation info)
+    {
+        m_choice.Item2Image = info.sprite;
+        DisplayUpgrade2(info.statistics);
+    }
+    void DisplayUpgrade2(ItemStatistics stats)
+    {
+        string text = "";
+        m_upgrade.maxFuel = Mathf.Round(Random.Range(0, stats.maxFuel) - m_current.maxFuel);
+        m_upgrade.maxVelocity = Mathf.RoundToInt(Random.Range(0, stats.maxVelocity) - m_current.maxVelocity);
+        m_upgrade.maxPingSpeed = Mathf.RoundToInt(Random.Range(0, stats.maxPingSpeed) - m_current.maxPingSpeed);
+        
+        if (m_upgrade.maxFuel != 0) text += (m_upgrade.maxFuel < 0 ? "" : "+") + m_upgrade.maxFuel + " fuel\n";
+        if (m_upgrade.maxVelocity != 0) text += (m_upgrade.maxVelocity < 0 ? "" : "+") + m_upgrade.maxVelocity + " velocity\n";
+        if (m_upgrade.maxPingSpeed != 0) text += (m_upgrade.maxPingSpeed < 0 ? "" : "+") + m_upgrade.maxPingSpeed + " ping\n";
+
+        m_choice.Item2Text = text;
+    }
+    
+    [System.Serializable]
+    public class ItemStatistics
+    {
+        public float maxFuel = 0;
+        public float maxVelocity = 0;
+        public float maxPingSpeed = 0;
+    };
+    [System.Serializable]
+    public class ItemInformation
+    {
+        public string name = "";
+        public Sprite sprite = null;
+        public ItemStatistics statistics = new ItemStatistics();
+    }
+}
